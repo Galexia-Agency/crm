66203285253ad08045e6d99b30ec7e3641f88624
Joe Bailey <joe@joebailey.xyz>
2022-11-11 11:36:21 +0000

    Fixed webworker

--- workers/projectSSE.js
+++ workers/projectSSE.js
@@ -1,9 +1,9 @@
 import EventSourcePolyfill from 'eventsource'
 
 onmessage = (e) => {
-  const url = e.data[1]
-  const id = e.data[2]
-  const authToken = e.data[3]
+  const url = e.data[0]
+  const id = e.data[1]
+  const authToken = e.data[2]
   const sse = new EventSourcePolyfill(url, {
     headers: {
       Authorization: authToken
